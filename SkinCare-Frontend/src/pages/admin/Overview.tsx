b8ab3ab454b2d3fcbe287ac5d4deabc142939cea
Zawe Yan Naing <zaweyannaing121.phys@gmail.com>
2025-08-30 22:32:35 +0630

    fix:err
    Signed-off-by: Zawe Yan Naing <zaweyannaing121.phys@gmail.com>

--- SkinCare-Frontend/src/pages/admin/Overview.tsx
+++ SkinCare-Frontend/src/pages/admin/Overview.tsx
@@ -2,8 +2,17 @@
 import { OverviewChart } from '@/components/charts/overview-chart';
 import { RecentSales } from '@/components/recent-sales';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
+import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { Activity, CreditCard, DollarSign, Users } from 'lucide-react';
+import { Activity, CreditCard, DollarSign, Download, Users } from 'lucide-react';
+import React from 'react';
+import { ChevronDownIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 function Overview() {
   const reportCards = [
@@ -28,6 +37,8 @@ function Overview() {
       icon: Activity,
     },
   ];
+  const [open, setOpen] = React.useState(false)
+  const [date, setDate] = React.useState<Date | undefined>(undefined)
 
   return (
     <div className="w-full max-w-7xl mx-auto p-6">
@@ -46,13 +57,35 @@ function Overview() {
             </BreadcrumbList>
           </Breadcrumb>
         </div>
-        <div className="flex items-center gap-2">
-          <select className="px-3 py-2 border rounded-md text-sm">
-            <option>Select date</option>
-          </select>
-          <button className="px-4 py-2 bg-green-600 text-white rounded-md text-sm font-medium">
-            Export
-          </button>
+        <div className="flex items-center gap-3">
+        <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date"
+            className="w-35 justify-between font-normal"
+          >
+            {date ? date.toLocaleDateString() : "Select date"}
+            <ChevronDownIcon />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            captionLayout="dropdown"
+            onSelect={(date) => {
+              setDate(date)
+              setOpen(false)
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+         <Button>
+         
+          <Download />
+          Export
+         </Button>
         </div>
       </div>
 
